bd7260d1bdf3b574b1ae771031295c9f37670b2a
Athiruj.K <athirujkaewseesuk@gmail.com>
2024-08-12 00:36:01 +0700

    add ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+
+	if (!needle)
+		return ((char *)haystack);
+	i = ft_strlen(needle);
+	while (len >= i
+		&& *haystack
+		&& ft_strncmp((char *)haystack, (char *)needle, i))
+	{
+		haystack++;
+		len--;
+	}
+	if (!*haystack || len < i)
+		return (NULL);
+	return ((char *)haystack);
+}
