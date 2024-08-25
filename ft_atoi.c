12240a80775cc12561979c761cab7c48f2101668
Athiruj.K <athirujkaewseesuk@gmail.com>
2024-08-25 17:08:49 +0700

    fix ft_atoi oparetor

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,13 @@
 /*   By: athi <athi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 13:55:27 by athi              #+#    #+#             */
-/*   Updated: 2024/08/24 13:55:29 by athi             ###   ########.fr       */
+/*   Updated: 2024/08/25 17:07:38 by athi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(char *str)
+int	ft_atoi(const char *str)
 {
 	short	m;
 	int		tmp;
@@ -21,9 +21,11 @@ int	ft_atoi(char *str)
 	m = 1;
 	while (ft_isspace(*str))
 		str++;
-	while (*str == '-' || *str == '+')
+	if (*str == '-' || *str == '+')
 		if (*str++ == '-')
 			m *= -1;
+	if (*str == '-' || *str == '+')
+		return (0);
 	while (*str >= '0' && *str <= '9')
 		tmp = tmp * 10 + (*str++ - '0');
 	return (tmp * m);
