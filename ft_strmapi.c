f26a6e9990cbc20a391637366e0fa25f645c4e2e
Athiruj.K <athirujkaewseesuk@gmail.com>
2024-08-27 10:04:16 +0700

    fix ft_strmapi Null at the end

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,25 +6,28 @@
 /*   By: athi <athi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 13:23:41 by athi              #+#    #+#             */
-/*   Updated: 2024/08/24 16:28:46 by athi             ###   ########.fr       */
+/*   Updated: 2024/08/27 10:02:57 by athi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char)){
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
 	char	*tmp;
 	size_t	i;
 
+	if (!s)
+		return (NULL);
 	tmp = (char *)malloc(ft_strlen(s) + 1);
 	if (!tmp)
 		return (NULL);
 	i = 0;
-	while (s[i] != '\0')
+	while (*s)
 	{
-		*(tmp + i) = (*f)(i, *(s + i));
+		*(tmp + i) = (*f)(i, *s++);
 		i++;
 	}
-	*tmp = '\0';
+	*(tmp + i) = '\0';
 	return (tmp);
 }
